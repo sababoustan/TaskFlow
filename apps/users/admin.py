3f8ee7a1b7ea43b79128f51482e4994ef25ed2ac
sababoustan <sababoostan230@gmail.com>
2026-07-01 18:19:48 +0330

    feat(users): implement custom user model and admin configuration

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 # Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('email', 'full_name', 'is_superuser', 'is_active',
+                    'is_verified')
+    list_filter = ('is_superuser', 'is_active', 'is_staff', 'is_verified')
+    search_fields = ('email', 'full_name')
+    ordering = ('email',)
+    fieldsets = (
+        ('Authentication', {
+            "fields": (
+                'email', 'password'
+            ),
+        }),
+        ("Personal Info", {
+            "fields": ("full_name",),
+        }),
+        ('Permissions', {
+            "fields": (
+                'is_staff', 'is_active', 'is_superuser', 'is_verified'
+            ),
+        }),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'full_name', 'password1', 'password2',
+                       'is_staff', 'is_active', 'is_superuser',),
+            }),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,50 @@
 from django.db import models
-
+from django.contrib.auth.models import (BaseUserManager,
+                                        AbstractBaseUser,
+                                        PermissionsMixin)
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_('The email must be set.'))
+        if not password:
+            raise ValueError(_("Password must be set."))
+        email = self.normalize_email(email).lower()
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_active', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True'))
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    full_name = models.CharField(max_length=255, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+    
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.full_name or self.email
+    
